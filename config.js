da157458b17ebf57b7c791226821bd2b69056e01
ken <ntkkenng@gmail.com>
2025-05-01 23:20:50 -0700

    minor change to port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const PORT = +process.env.PORT || 3000;
+const PORT = +process.env.PORT || 3001;
 
 function getDatabaseUrl() {
 	return process.env.NODE_ENV === "test"
